10595d26463d02d91b59838ae4cef1b792500359
npolak51 <nick.polak.51@gmail.com>
2025-12-04 14:26:34 -0800

    Integrate PlayerPage component into App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import PlayerPage from './Player/PlayerPage'
+
+function App() {
+  return <PlayerPage />
+}
+
+export default App
 function App() {
   const [count, setCount] = useState(0)
 
